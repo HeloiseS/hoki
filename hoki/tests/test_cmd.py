c467aa7e679a088810452f345850bafadb6158a4
Heloise <HeloiseS@users.noreply.github.com>
2019-09-26 14:28:14 +1200

    Update test_cmd.py

--- hoki/tests/test_cmd.py
+++ hoki/tests/test_cmd.py
@@ -1,8 +1,10 @@
 from hoki.cmd import CMD
 import pkg_resources
+import os 
 
 data_path = pkg_resources.resource_filename('hoki', 'data')
 
+print(os.listdir(data_path+'/sample_stellar_models/'))
 
 class TestCMD(object):
     cmd = CMD(data_path+'/input_bpass_z020_bin_imf135_300', path = data_path+'/sample_stellar_models/')
