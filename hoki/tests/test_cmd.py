31655b95ce383998b2bbc639b7c908a68c4fdcb6
Heloise <HeloiseS@users.noreply.github.com>
2019-09-26 15:04:04 +1200

    Update test_cmd.py

--- hoki/tests/test_cmd.py
+++ hoki/tests/test_cmd.py
@@ -3,20 +3,23 @@ import pkg_resources
 import os 
 
 data_path = pkg_resources.resource_filename('hoki', 'data')
-print(os.listdir(data_path))
+
 #print(os.listdir(data_path+'/sample_stellar_models/'))
 
 class TestCMD(object):
     cmd = CMD(data_path+'/input_bpass_z020_bin_imf135_300', path = data_path+'/sample_stellar_models/')
 
     def test_init_(self ):
+        print(os.listdir(data_path))
         assert self.cmd is not None, "object not instanciated"
         assert sum(self.cmd.grid.flatten()) == 0, "CMD Grid should be empty"
 
     def test_make(self):
+        print(os.listdir(data_path))
         self.cmd.make(filter1='B', filter2='V')
         assert sum(self.cmd.grid.flatten()) == 0, 'CMD grid is not empty'
         #assert sum(self.cmd.grid.flatten()) != 0, 'CMD grid is still empty'
 
     def test_plot(self):
+        print(os.listdir(data_path))
         myplot = self.cmd.plot(log_age=7)
