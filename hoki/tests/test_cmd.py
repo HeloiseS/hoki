5eb848b6ac67343a3fe602136b4399ceca8bda33
heloises <heloisefw@gmail.com>
2020-03-25 16:08:42 +1300

    "CMD now allows you to chose the magnitude filter so make function has changed."

--- hoki/tests/test_cmd.py
+++ hoki/tests/test_cmd.py
@@ -8,18 +8,28 @@ cmd_path = data_path+'/cmd_bv_z002_bin_imf135_300'
 
 
 class TestCMD(object):
+
+    def test_plot_pickle(self):
+        cmd = unpickle(cmd_path)
+        myplot = cmd.plot(log_age=7)
+        myplot.set_xlabel('bla')
+        del myplot
+        del cmd
+
     cmd = CMD(data_path+'/input_bpass_z020_bin_imf135_300')
 
     def test_init_(self):
         #print(os.listdir(data_path))
         assert self.cmd is not None, "object not instanciated"
-        assert sum(self.cmd.grid.flatten()) == 0, "CMD Grid should be empty"
-    """
+        assert self.cmd.grid is None, "CMD Grid should be None"
+
     # NOTE: These tests are to do locally in dev - they require 50GB of full model data to test
     # they cannot easily be implemented into Travis CI.
+
+    """   
     def test_make(self):
         print(os.listdir(data_path))
-        self.cmd.make(filter1='B', filter2='V')
+        self.cmd.make(mag_filter='V', col_filters=['B', 'V'])
         assert sum(self.cmd.grid.flatten()) != 0, 'CMD grid is still empty'
 
     def test_plot(self):
@@ -28,11 +38,8 @@ class TestCMD(object):
 
     def test_plot_bin0(self):
         self.cmd.plot(log_age=6.0)
+
+    def test_at_log_age(self):
+        self.cmd.at_log_age(log_age=6.8)
     """
 
-    def test_plot_pickle(self):
-        cmd = unpickle(cmd_path)
-        myplot = cmd.plot(log_age=7)
-        myplot.set_xlabel('bla')
-        del myplot
-        del cmd
