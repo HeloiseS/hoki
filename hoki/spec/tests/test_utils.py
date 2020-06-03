22c6396db3c6381fa2c50400f92e1b97d5adda5e
heloises <heloisefw@gmail.com>
2020-06-03 13:48:39 +1200

    added EW utilities and tests

--- hoki/spec/tests/test_utils.py
+++ hoki/spec/tests/test_utils.py
@@ -1,19 +1,40 @@
 import pandas as pd
 from hoki import load
-from hoki.spec.utils import dopcor
+from hoki.spec import utils as su
 import numpy as np
 import os
 import pkg_resources
+import pytest
+from hoki.utils.exceptions import HokiFatalError, HokiUserWarning, HokiFormatError, HokiFormatWarning
 
 data_path = pkg_resources.resource_filename('hoki', 'data')
 
-#wavelengths and throughput/transmission manually read from the LICK.LICK.U file
-wl = [3100,3200,3300,3400,3500,3600,3700,3800,3900,4000,4100]
-transmission = [0.015, 0.140,0.347,0.440,0.625,0.685,0.708,0.643,0.458,0.170,0.029]
-data = load.model_output(f"{data_path}/spectra-bin-imf135_300.z002.dat")
+spectra = load.model_output(data_path+'/spectra-bin-imf135_300.z008.dat')
+spectra_b=spectra[(spectra.WL>4000) & (spectra.WL<6000)]
+lower, line_bound, upper =[5743,5760], [5760,5855], [5855,5872]
+
 
 def test_dopcor():
     ngc4993 = pd.read_csv(data_path+'/ngc4993_spec_tot.dat')
     wl0_init = ngc4993.wl[0]
-    dopcor(ngc4993,  0.009783)
+    su.dopcor(ngc4993, 0.009783)
     assert np.isclose((wl0_init-ngc4993.wl[0]), 46.4655, atol=1e-05), "The doppler correction isn't right"
+
+
+def test_pseudo_continuum():
+    cont = su.pseudo_continuum(spectra_b.WL, spectra_b['6.6'], lower, upper)
+    assert np.isclose(cont[0], 12099.23805719865), "Continuum is wrong"
+
+
+def test_error_pseudo_continuum():
+    with pytest.raises(HokiFormatError):
+        __, __ = su.pseudo_continuum(spectra_b.WL, spectra_b['6.6'], '2', upper)
+
+
+def test_equivalent_width_line_bound():
+    ew = su.equivalent_width(spectra_b.WL, spectra_b['6.6'], lower, upper, line_bound)
+    assert np.isclose(ew, -9.727277922413643), "EW is wrong"
+
+def test_equivalent_width_no_line_bound():
+    ew = su.equivalent_width(spectra_b.WL, spectra_b['6.6'], lower, upper)
+    assert np.isclose(ew, -9.727277922413643), "EW is wrong"
--- hoki/spec/utils.py
+++ hoki/spec/utils.py
@@ -4,7 +4,7 @@ especially BPASS synthetic spectra
 """
 
 import numpy as np
-import pandas as pd
+from hoki.utils.exceptions import HokiFatalError, HokiUserWarning, HokiFormatError, HokiFormatWarning
 
 def dopcor(df, z, wl_col_index=0):
     """
@@ -18,3 +18,76 @@ def dopcor(df, z, wl_col_index=0):
     wl_dopcor = (df.iloc[:, wl_col_index].values) - (df.iloc[:, wl_col_index].values * z)
     df.iloc[:, wl_col_index] = wl_dopcor
     return
+
+#TODO: Unittests
+def pseudo_continuum(wl, spectrum, lower, upper):
+    """
+    Traces a linear pseudo-continuum
+
+    Parameters
+    ----------
+    wl
+    spectrum
+    lower
+    upper
+
+    Returns
+    -------
+
+    """
+    # Calculating the mean Flux of the lower continuum region
+    try:
+        cont1 = np.mean(spectrum[(wl > lower[0]) & (wl < lower[1])])
+    except TypeError as e:
+        raise HokiFormatError(f'{e} DEBUGGING ASSISTANT: Check your input types!')
+
+    # Finding the middle wavelength of the lower continuum region
+    wl1 = np.mean(lower)
+
+    # Calculating the mean Flux of the upper continuum region
+    try:
+        cont2 = np.mean(spectrum[(wl > upper[0]) & (wl < upper[1])])
+    except TypeError as e:
+        raise HokiFormatError(f'{e} DEBUGGING ASSISTANT: Check your input types!')
+
+    # Finding the middle wavelength of the upper continuum region
+    wl2 = np.mean(upper)
+
+    # Calculates the gradient and intercept...
+    m = (cont2 - cont1) / (wl2 - wl1)
+    c = cont2 - wl2 * m
+
+    # An then calculates the line of the continuum for the whole wavelength range.
+    pseudo_cont = m * wl + c
+
+    return pseudo_cont.values
+
+
+def equivalent_width(wl, spectrum, lower_cont, upper_cont, line_bound=None):
+    """
+    Measure of the equivalent width
+
+    Parameters
+    ----------
+    wl
+    spectrum
+    lower_cont
+    upper_cont
+    line_bound
+
+    Returns
+    -------
+
+    """
+
+    # Calculates the pseudo continuum
+    cont = pseudo_continuum(wl, spectrum, upper_cont, lower_cont)
+
+    # Masking areas outside of the line we care about
+    if line_bound is None:
+        mask = (wl > lower_cont[1]) & (wl < upper_cont[0])
+    else:
+        assert isinstance(line_bound, list), "not list"
+        mask = (wl > line_bound[0]) & (wl < line_bound[1])
+
+    return np.sum(1 - spectrum[mask] / cont[mask])
