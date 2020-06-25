aa7f915b6006009a5f5fd74ab2446afedab7ec39
heloises <heloisefw@gmail.com>
2020-06-25 16:52:23 +1200

    added spec back

---
+++ hoki/spec.py
@@ -0,0 +1,17 @@
+"""
+Module to hold functions and utilities to be applied to spectra,
+especially BPASS synthetic spectra
+"""
+
+
+def dopcor(df, z, wl_col_index=0):
+    """
+    Basis doppler correction for hoki's dataframes
+    
+    Notes
+    -----
+    The correction is applied IN PLACE.
+    """
+    wl_dopcor = (df.iloc[:, wl_col_index].values) - (df.iloc[:, wl_col_index].values * z)
+    df.iloc[:, wl_col_index] = wl_dopcor
+    return
