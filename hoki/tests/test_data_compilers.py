224af192e152c0833c8617c4cb4553a2b5cc61d5
Max Briel <max.briel@gmail.com>
2020-06-26 14:03:06 +1200

    Add SpectraCompiler test
    Uses `unittest.mock` to allow for a smaller DataFrame to be used.

--- hoki/tests/test_data_compilers.py
+++ hoki/tests/test_data_compilers.py
@@ -2,36 +2,50 @@
 Tests for the data_compiler package
 """
 
-import os
-import shutil
+import os.path
 import pkg_resources
 import pandas as pd
 import numpy as np
 import numpy.testing as npt
 from hoki.data_compilers import SpectraCompiler
 from hoki.load import model_output
-from hoki.constants import BPASS_METALLICITIES, BPASS_NUM_METALLICITIES
-from unittest.mock import Mock, patch
+from hoki.constants import BPASS_NUM_METALLICITIES
+from unittest.mock import patch
 
 data_path = pkg_resources.resource_filename('hoki', 'data')
 
-
 class TestSpectraCompiler(object):
+
+    # Initialise a smaller pandas dataframe
     columns = pd.MultiIndex.from_product([BPASS_NUM_METALLICITIES, np.linspace(1, 10, 10)],
                                          names=["Metallicicty", "Wavelength"])
     df = pd.DataFrame(index=np.linspace(6,11, 51), columns=columns, dtype=np.float64)
-    data = model_output(f"{data_path}/spectra-bin-imf135_300.z002.dat").loc[:, slice("6.0", "11.0")]
-    def model_output_slice(self, value):
-        return self.data
 
+    # Initialise model_output DataFrame return a smaller single dataframe
+    # This reduces I/O readings
+    data = model_output(f"{data_path}/spectra-bin-imf135_300.z002.dat").loc[:, slice("6.0", "11.0")]
+
+    # Patch the Dataframe creation function
     @patch("hoki.data_compilers.pd.DataFrame")
+    # Patch the model_output function
     @patch("hoki.data_compilers.model_output")
-    def test_compiler(self, mock_data_compilers, mock_pd):
-        print(self.data)
-        mock_pd.return_value = self.df
-        mock_data_compilers.side_effect = self.model_output_slice
+    def test_compiler(self, mock_model_output, mock_dataframe):
+
+        # Set the smaller output DataFrame as pandas DataFrame creation output
+        mock_dataframe.return_value = self.df
+
+        # Set the model_output to the DataFrame
+        mock_model_output.return_value = self.data
+
 
         spec = SpectraCompiler(f"{data_path}",
                                f"{data_path}",
                                "imf135_300")
+
+        # Check if pkl file is created
+        assert os.path.isfile(f"{data_path}/all_spectra-bin-imf135_300.pkl")
+        # Check output dataframe
         npt.assert_allclose(spec.spectra.loc[:,(0.002,slice(0,10))], self.data.T)
+
+        # Remove created pickle
+        os.remove(f"{data_path}/all_spectra-bin-imf135_300.pkl")
