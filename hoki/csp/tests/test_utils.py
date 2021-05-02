6fa186c7c891070a86dea4a3fdd464b80e8f4508
Max Briel <max.briel@auckland.ac.nz>
2021-05-03 09:27:52 +1200

    Removed spectra calculation before release

--- hoki/csp/tests/test_utils.py
+++ hoki/csp/tests/test_utils.py
@@ -134,41 +134,6 @@ class TestRateCalculations(object):
             8, out, err_msg="_at_time has failed for a past time.")
 
 
-class TestSpectraCalculations(object):
-
-    edges = np.linspace(0, 10, 11)
-    Z_values = np.zeros(10) + 0.00001
-    mass_values = np.zeros(10) + 1
-
-    def test_over_time(self):
-        spectra = np.zeros((13, 51, 100000)) + 1
-        out = utils._over_time_spectrum(self.Z_values,
-                                        self.mass_values,
-                                        self.edges,
-                                        spectra)
-        npt.assert_allclose(out[4], np.zeros(100000) + 6,
-                            err_msg="_over_time_spectra calculation has failed.")
-
-    def test_at_time_now(self):
-        spectra = np.zeros((13, 51, 100000)) + 1
-        out = utils._at_time(self.Z_values,
-                                      self.mass_values,
-                                      self.edges,
-                                      spectra)
-        print(out)
-        npt.assert_allclose(out, np.zeros(100000) + 10,
-                            err_msg="_at_time spectrum calculation has failed for the now.")
-
-    def test_at_time_past(self):
-        spectra = np.zeros((13, 51, 100000)) + 1
-        out = utils._at_time(self.Z_values[5:],
-                                      self.mass_values[5:],
-                                      self.edges[5:],
-                                      spectra)
-        npt.assert_allclose(out, np.zeros(100000) + 5,
-                            err_msg="_at_time spectrum calculation has failed for in the past.")
-
-
 ###############################
 #    TEST HELPER FUNCTIONS    #
 ###############################
