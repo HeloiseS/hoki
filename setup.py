14ef5d7e10edd75f41b8fa7ee17be1b97391eaad
heloises <heloisefw@gmail.com>
2022-10-06 12:54:03 +0200

    tidied up something and updated version

--- setup.py
+++ setup.py
@@ -59,7 +59,7 @@ try:
 except Exception:
     version = '{version}'
 """.lstrip()
-setup(version='1.7.0')
+setup(version='1.7.1')
 #setup(use_scm_version={'write_to': os.path.join('hoki', 'version.py'),
 #                       'write_to_template': VERSION_TEMPLATE})
       #,ext_modules=get_extensions())
